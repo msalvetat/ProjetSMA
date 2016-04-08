d4f2a507f1bc35a4e74766a079c8be7ce3310af6
Marjorie Salvetat <marjorie.salvetat@gmail.com>
2016-04-08 11:47:41 +0200

    Test if the step perceive of a teacher is working (TODO : implement) #1

---
+++ src/test/groovy/m2dl/jlm/projetsma/agent/TeacherAgentTest.groovy
@@ -0,0 +1,25 @@
+package m2dl.jlm.projetsma.agent
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+class TeacherAgentTest {
+
+	def 'check the step perceive'() {
+
+		given:
+		//Cours cours = new Cours("pcr");
+		//TeacherAgent agent = new TeacherAgent("sheogorath",cours);
+		//SMA sma = new SMA();
+		//Ajouter l'agent dans le sma
+
+		when:
+		//Execution du sma et donc de l'agent
+
+		then:
+		//Le knowledge de l'agent est bien à jour
+		assert false
+	}
+	
+}
