6cfb28cb9b40d3783b0b70a35e3655042c0b6375
chj0455a <julien.chaloupy@univ-tlse3.fr>
2016-04-10 20:58:49 +0200

    #1 added decideAndAct beahviour and corresponding test

--- src/main/java/m2dl/jlm/projetsma/agent/TeacherAgent.java
+++ src/main/java/m2dl/jlm/projetsma/agent/TeacherAgent.java
@@ -2,26 +2,29 @@ package m2dl.jlm.projetsma.agent;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import fr.irit.smac.libs.tooling.scheduling.contrib.twosteps.ITwoStepsAgent;
 import m2dl.jlm.projetsma.agent.knowledge.Knowledge;
 import m2dl.jlm.projetsma.environment.Environment;
 import m2dl.jlm.projetsma.environment.Room;
 
-public class TeacherAgent implements ITwoStepsAgent{
+public class TeacherAgent implements ITwoStepsAgent {
 
 	Knowledge knowledge;
 	String id;
-	
+
 	public TeacherAgent(String id) {
 		this.id = id;
 		this.knowledge = new Knowledge();
 	}
-	
+
 	public void perceive() {
-		
+
 		HashSet<Room> rooms = (HashSet<Room>) Environment.getInstance().getRooms();
-		HashMap<Room,TeacherAgent> allocationsTeacherRoom = (HashMap<Room, TeacherAgent>) Environment.getInstance().getAllocationsTeacherRoom();
+		HashMap<Room, TeacherAgent> allocationsTeacherRoom = (HashMap<Room, TeacherAgent>) Environment.getInstance()
+				.getAllocationsTeacherRoom();
 		knowledge.setRooms(rooms);
 		knowledge.setAllocationsTeacherRoom(allocationsTeacherRoom);
 	}
@@ -31,8 +34,22 @@ public class TeacherAgent implements ITwoStepsAgent{
 	}
 
 	public void decideAndAct() {
-		// TODO Auto-generated method stub
-		
+		boolean hasDecided = false;
+		Set<Room> rooms = this.knowledge.getRooms();
+		Iterator<Room> iterator = rooms.iterator();
+		Room room;
+		HashMap<Room, TeacherAgent> allocationsTeacherRoom = this.knowledge.getAllocationsTeacherRoom();
+		while(iterator.hasNext() && !hasDecided) {
+			room = iterator.next();
+			
+			// Decide : we decide the room we can take
+			if (!allocationsTeacherRoom.containsKey(room)) {
+				
+				// Act : booking the room
+				allocationsTeacherRoom.put(room, this);
+				hasDecided = true;
+			}
+		}
 	}
 
 }
--- src/test/groovy/m2dl/jlm/projetsma/agent/TeacherAgentTest.groovy
+++ src/test/groovy/m2dl/jlm/projetsma/agent/TeacherAgentTest.groovy
@@ -1,8 +1,11 @@
 package m2dl.jlm.projetsma.agent
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.concurrent.Executors
 
 import m2dl.jlm.projetsma.agent.knowledge.Knowledge
+import m2dl.jlm.projetsma.environment.Environment;
 import m2dl.jlm.projetsma.environment.Room
 import spock.lang.Specification
 import fr.irit.smac.libs.tooling.scheduling.contrib.twosteps.ITwoStepsAgent
@@ -25,6 +28,25 @@ class TeacherAgentTest extends Specification {
 		Knowledge knowledge = teacherSMA.getKnowledge()
 		knowledge.getRooms().size() == 3
 		knowledge.getAllocationsTeacherRoom().size() == 0
+	}
+
+	def 'check the step decideAndAct for a teacher agent'() {
+
+		given: // GIVEN 3 rooms and 1 already allocated rooms
+		TwoStepsSystemStrategy systemStrategy = new TwoStepsSystemStrategy(new HashSet<ITwoStepsAgent>(),Executors.newFixedThreadPool(8));
+		TeacherAgent teacher = new TeacherAgent("sheogorath");
+		Room room1 = teacher.getKnowledge().getRooms().iterator().next();
+		teacher.getKnowledge().getAllocationsTeacherRoom().put(room1, null);
+		systemStrategy.addAgent(teacher);
+
+		when: // WHEN the system is running
+		systemStrategy.doStep();
+
+		then:
+		TeacherAgent teacherSMA = systemStrategy.getAgents().first()
+		Knowledge knowledge = teacherSMA.getKnowledge()
+		knowledge.getAllocationsTeacherRoom().size() == 2
+		knowledge.getAllocationsTeacherRoom().get(room1) == teacher
 	}
 	
 }
